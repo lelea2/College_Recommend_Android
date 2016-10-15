8565536ab7950eefca40b268e5758eb9cc0f719a
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-14 22:20:26 -0700

    Initial check in for state dropdown call

--- app/src/main/java/com/kdao/college_recommend_android/MainActivity.java
+++ app/src/main/java/com/kdao/college_recommend_android/MainActivity.java
@@ -14,29 +14,113 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.os.AsyncTask;
+import android.app.ProgressDialog;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner state_spinner;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         state_spinner = (Spinner) findViewById(R.id.state_spinner);
-        List<String> categories = new ArrayList<String>();
-        categories.add("Automobile");
-        categories.add("Business Services");
-        categories.add("Computers");
-        categories.add("Education");
-        categories.add("Personal");
-        categories.add("Travel");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-        // Drop down layout style - list view with radio button
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Get data
+        getState();
+    }
 
-        // attaching data adapter to spinner
-        state_spinner.setAdapter(dataAdapter);
+    //Loading to get state
+    private void getState() {
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(MainActivity.this, "", "Loading...");
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet("http://college-recommended.herokuapp.com/data/states");
+                try {
+                    try {
+                        System.out.println(">>>> Get state successfully");
+                        HttpResponse httpResponse = httpClient.execute(httpGet);
+                        InputStream inputStream = httpResponse.getEntity().getContent();
+                        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                        StringBuilder stringBuilder = new StringBuilder();
+                        String bufferedStrChunk = null;
+                        while((bufferedStrChunk = bufferedReader.readLine()) != null) {
+                            stringBuilder.append(bufferedStrChunk);
+                        }
+                        return stringBuilder.toString();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (Exception uee) {
+                    System.out.println("An Exception given because of UrlEncodedFormEntity argument :" + uee);
+                    uee.printStackTrace();
+                }
+                return null;
+            }
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                progressDialog.dismiss();
+                try {
+                    JSONArray arrayObj = null;
+                    JSONParser jsonParser = new JSONParser();
+                    arrayObj= (JSONArray) jsonParser.parse(result);
+                    populateStates(arrayObj);
+                } catch(Exception ex) {
+                    System.out.println(ex);
+                }
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute();
+    }
+
+    //private function to popular state view
+    private void populateStates(JSONArray arrayObj) {
+        if (arrayObj.size() > 0) {
+            List<String> states = new ArrayList<String>();
+            for (int i = 0; i < arrayObj.size(); i++) {
+                try {
+                    JSONObject object = (JSONObject) arrayObj.get(i);
+                    states.add(object.get("name").toString());
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+            ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, states);
+            // Drop down layout style - list view with radio button
+            dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // attaching data adapter to spinner
+//            state_spinner.setAdapter(dataAdapter);
+            state_spinner.setPrompt("Select your state");
+            state_spinner.setAdapter(new NothingSelectedSpinnerAdapter(
+                            dataAdapter,
+                            R.layout.state_spinner_row_nothing_selected,
+                            // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                            this));
+        }
     }
 }
